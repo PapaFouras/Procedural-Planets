65685c0b382ca58f22112103dbbcb3fac26e3502
PapaFouras <nicolasg94@hotmail.fr>
2021-04-28 20:46:03 +0200

    Ep 03

--- Assets/Script/ShapeGenerator.cs
+++ Assets/Script/ShapeGenerator.cs
@@ -6,12 +6,39 @@ public class ShapeGenerator
 {
   ShapeSettings shapeSettings;
 
+ 
+  NoiseFilter[] noiseFilters;
+
     public ShapeGenerator(ShapeSettings shapeSettings)
     {
         this.shapeSettings = shapeSettings;
+
+        noiseFilters = new NoiseFilter[shapeSettings.noiseLayers.Length];
+        for (int i = 0; i < noiseFilters.Length; i++)
+        {
+            noiseFilters[i] = new NoiseFilter(shapeSettings.noiseLayers[i].noiseSettings);
+        }
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere){
-        return pointOnUnitSphere* shapeSettings.planetRadius;
+        float firstLayerValue = 0;
+        float elevation = 0;
+
+        if(noiseFilters.Length > 0){
+            firstLayerValue = noiseFilters[0].Evaluate(pointOnUnitSphere);
+            if(shapeSettings.noiseLayers[0].enabled){
+                elevation = firstLayerValue;
+            }
+        }
+        for (int i = 1; i < noiseFilters.Length; i++)
+        {
+            if(shapeSettings.noiseLayers[i].enabled){
+
+                float mask = shapeSettings.noiseLayers[i].useFirstLayerAsAMask ? firstLayerValue : 1;
+            elevation += noiseFilters[i].Evaluate(pointOnUnitSphere);
+
+            }
+        }
+        return pointOnUnitSphere* shapeSettings.planetRadius * (1+elevation);
     }
 }
--- Assets/Script/ShapeSettings.cs
+++ Assets/Script/ShapeSettings.cs
@@ -6,4 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ShapeSettings", menuName = "Planets/ShapeSettings", order = 0)]
 public class ShapeSettings : ScriptableObject {
     public float planetRadius = 1;
+
+    public NoiseLayer[] noiseLayers;
+    
+    [System.Serializable]
+
+    public class NoiseLayer{
+        public bool enabled = true;
+        public bool useFirstLayerAsAMask;
+        public NoiseSettings noiseSettings;
+    }
 }
